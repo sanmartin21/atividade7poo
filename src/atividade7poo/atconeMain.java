91404f8b5c525982491e37d519b51d030b631662
sanmartin21 <jvsanmartin@yahoo.com.br>
2021-10-21 07:50:15 -0300

    at07

--- src/atividade7poo/atconeMain.java
+++ src/atividade7poo/atconeMain.java
@@ -16,7 +16,7 @@ public class atconeMain {
 		System.out.println("Digite o altura do cone a ser pintado: ");
 		atcone.setAltura(input.nextFloat());
 
-		System.out.println("Escolha o tipo de tinta que você quer? 1-Tinta Clase1, 2-Tinta Classe2,3-Tinta Classe3");
+		System.out.println("Escolha o tipo de tinta que você quer? 1-Tinta Clase1, 2-Tinta Classe2,3-Tinta Classe3: ");
 		atcone.setTinta(input.nextInt());
 		
 		System.out.println(atcone);
